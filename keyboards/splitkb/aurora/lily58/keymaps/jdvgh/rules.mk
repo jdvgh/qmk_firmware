e751ca2aa3427d32fd8b9db490cefc09c979ce81
jdvgh <133217951+jdvgh@users.noreply.github.com>
2023-09-20 19:00:01 +0200

    feat: Add Aurora Lily58 Miryoku layout (still WIP)

---
+++ keyboards/splitkb/aurora/lily58/keymaps/jdvgh/rules.mk
@@ -0,0 +1,18 @@
+# Build Options
+#   change to "no" to disable the options, or define them in the Makefile in
+#   the appropriate keymap folder that will get included automatically
+#
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE         = yes
+CONSOLE_ENABLE          = no
+COMMAND_ENABLE          = no
+
+RGBLIGHT_ENABLE         = no
+RGB_MATRIX_ENABLE       = no
+
+OLED_ENABLE     = yes
+
+
+
+CONVERT_TO=liatris
+SPLIT_TRANSPORT         = no
